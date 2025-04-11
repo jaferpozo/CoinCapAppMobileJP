c2615f61b5b4bfe1c1ff9ea582480962af292ece
jaferpozo <jaferpozo@gmail.com>
2025-04-10 22:08:53 -0500

    Navigation 10-04-2025

---
+++ app/src/main/java/com/example/coincapappjp/AppModule.kt
@@ -0,0 +1,39 @@
+package com.example.coincapappjp
+
+import com.example.coincapappjp.services.CoinCapApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import javax.inject.Singleton
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun ktorClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun coinCapApiService(client: HttpClient): CoinCapApiService {
+        return CoinCapApiService(client)
+    }
+}
--- app/src/main/java/com/example/coincapappjp/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincapappjp/services/CoinCapApiService.kt
@@ -1,4 +1,5 @@
 package com.example.coincapappjp.services
+
 import com.example.coincapappjp.models.AssetsResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -10,7 +11,7 @@ class CoinCapApiService @Inject constructor(
     private val client: HttpClient
 ) {
     suspend fun getAssets(): AssetsResponse {
-        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=")
+        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=5afe9a3cc58cd3026d29a4dd7cec05ef0e1cef5a50072b547d8bcd62785ab287")
         return response.body()
     }
 }
