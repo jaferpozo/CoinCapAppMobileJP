cdd2771c27a5a23a71bc0c1b220870b06c805476
jaferpozo <jaferpozo@gmail.com>
2025-04-10 22:16:42 -0500

    NAvigation sin APIKEY

--- app/src/main/java/com/example/coincapappjp/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincapappjp/services/CoinCapApiService.kt
@@ -6,12 +6,11 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import javax.inject.Inject
-
 class CoinCapApiService @Inject constructor(
     private val client: HttpClient
 ) {
     suspend fun getAssets(): AssetsResponse {
-        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=")
+        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey= ")
         return response.body()
     }
 }
